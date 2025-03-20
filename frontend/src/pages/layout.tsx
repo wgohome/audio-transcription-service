4d60796e32528f9255b3fd837cf8b2d81cfc4d48
William Goh <wirriamm@gmail.com>
2025-03-20 21:29:45 +0800

    shadcn sonner

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import { Toaster } from "sonner";
 
 export default function Layout() {
   return (
     <div className="container mx-auto h-screen px-3">
       <Outlet />
+      <Toaster richColors />
     </div>
   );
 }
