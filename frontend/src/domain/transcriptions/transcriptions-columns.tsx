3040513629cdd79dba831cb61ab06779313fa128
William Goh <wirriamm@gmail.com>
2025-03-22 00:36:43 +0800

    column width

--- frontend/src/domain/transcriptions/transcriptions-columns.tsx
+++ frontend/src/domain/transcriptions/transcriptions-columns.tsx
@@ -11,13 +11,19 @@ export const columns: ColumnDef<Transcription>[] = [
   {
     accessorKey: "filename",
     header: "File Name",
+    size: 50,
+    cell: (props) => <span className="text-wrap">{props.getValue() as string}</span>,
   },
   {
     accessorKey: "transcribedText",
     header: "Transcription",
+    maxSize: 10,
+    cell: (props) => <span className="text-wrap">{props.getValue() as string}</span>,
   },
   {
     accessorKey: "createdAt",
     header: "Created at",
+    maxSize: 10,
+    cell: (props) => <span className="text-wrap">{JSON.stringify(props.getValue())}</span>,
   },
 ];
